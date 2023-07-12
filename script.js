b711687c320c90e0554ecc6907ed252548c96802
sahasrakalahasthi <84376556+sahasrakalahasthi@users.noreply.github.com>
2023-07-12 19:50:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+let cursor1=document.querySelector('.cursor-1');
+let cursor2=document.querySelector('.cursor-2');
+
+window.onmousemove=(e) => {
+    cursor1.style.top=e.pageY+'px';
+    cursor1.style.left=e.pageX+'px';
+    cursor2.style.top=e.pageY+'px';
+    cursor2.style.left=e.pageX+'px';
+}
+
+document.querySelectorAll('a').forEach(links=>{
+    links.onmouseenter=()=>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave=()=>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+});
+
+let sections=document.querySelectorAll('section');
+let navLinks=document.querySelectorAll('header nav a');
+
+window.onscroll=()=>{
+    sections.forEach(sec =>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+        if(top>=offset && top<offset+height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' +id+']').classList.add('active');
+            })
+        }
+    })
+let header=document.querySelector('.header');
+header.classList.toggle('sticky',window.scrollY>100);
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
+
+let darkmodeIcon=document.querySelector('#darkmode-icon');
+darkmodeIcon.onclick=()=>{
+    darkmodeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('darkmode');
+}
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ScrollReveal().reveal('.home-img img,.about-img img,.skills-container,.portfolio-box,.contact-form',{origin:'bottom'});
+ScrollReveal().reveal('.about-img img,.home-content h1',{origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-content,.home-content h3,.contact',{origin:'top'});
+var swiper=new Swiper(".mySwiper",{
+    slidesPerView:1,
+    spaceBetween:50,
+    loop:true,
+    grabCursor:true,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,     
+    },
+    navigation:{
+        nextEl:".swiper-button-next",
+        pervEl:".swiper-button-prev",
+    },
+});
+
